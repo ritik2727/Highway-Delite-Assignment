828b796f68be678d639f3b773a7d350867772c5d
ritik2727 <ritikjain2727@gmail.com>
2023-02-02 15:44:55 +0530

    add models

--- backend/data/product.js
+++ backend/data/product.js
@@ -1,67 +1,74 @@
 const products = [
   {
-    productName: "Ghost of Uchiha Round Neck T-Shirt",
+    name: "Ghost of Uchiha Round Neck T-Shirt",
     image:
       "https://images.bewakoof.com/t540/ghost-of-uchiha-men-s-t-shirt-387427-1634624585-1.jpg",
     price: "399",
+    countInStock: 10,
   },
   {
     price: "399",
-    productName: "Classic Flash logo Round Neck T-Shirt",
+    name: "Classic Flash logo Round Neck T-Shirt",
     image:
       "https://images.bewakoof.com/t540/classic-flash-logo-printed-t-shirts-fl-men-s-printed-t-shirts-368239-1626331170.jpg",
+      countInStock: 10,
   },
   {
-    productName: "Squid Triangle Half Sleeve T-Shirt",
+    name: "Squid Triangle Half Sleeve T-Shirt",
     image:
       "https://images.bewakoof.com/t540/squid-triangle-438601-1635769216-1.jpg",
     price: "299",
+    countInStock: 10,
   },
   {
-    productName: "Marvel Shield Raglan Contrast Polo",
+    name: "Marvel Shield Raglan Contrast Polo",
     price: "499",
     image:
       "https://images.bewakoof.com/t540/marvel-shield-raglan-polo-t-shirts-chili-pepper-men-s-printed-raglan-polo-t-shirts-325639-1618563575.jpg",
+      countInStock: 10,
   },
   {
-    productName: "Star Wars Round Neck Short Sleeves T Shirt",
+    name: "Star Wars Round Neck Short Sleeves T Shirt",
     price: "299",
     image:
       "https://images.bewakoof.com/t540/star-wars-by-wear-your-mind-star-wars-round-neck-short-sleeves-t-shirt-white-419540-1632912356-1.jpg",
+      countInStock: 10,
   },
   {
     price: "499",
-    productName: "Clan Nightmare Riun T-Shirt",
+    name: "Clan Nightmare Riun T-Shirt",
     image:
       "https://images.bewakoof.com/t540/clan-nightmare-men-s-t-shirts-387567-1634624545-1.jpg",
+      countInStock: 10,
   },
   {
-    productName: "MSD Forever Front-Back T-Shirt ",
+    name: "MSD Forever Front-Back T-Shirt ",
     price: "599",
     image:
       "https://images.bewakoof.com/t540/msd-forever-front-back-half-sleeve-t-shirt-black-men-s-printed-t-shirts-277954-1609158795.jpg",
+      countInStock: 10,
   },
   {
     image:
       "https://images.bewakoof.com/t540/batman-outline-logo-full-sleeve-t-shirt-bml-black-men-s-printed-full-sleeve-t-shirt-283891-1631798625.jpg",
-    oldPrice: "399",
-    productName: "Batman Outline Logo Full Sleeve T-Shirt ",
-    nu: "yes",
+
+    name: "Batman Outline Logo Full Sleeve T-Shirt ",
+   
     price: "299",
-    type: "dc",
-    key: "Rxb3KDTubfhohZOOBQ83",
+    countInStock: 10,
   },
   {
     price: "499",
     image:
       "https://images.bewakoof.com/t540/worthy-half-sleeve-t-shirt-avegl-253983-1635869223-1.jpg",
-    productName: "Worthy Half Sleeve T-Shirt (AVEGL)",
-    key: "ZPVzBpQahBtqgSU7X48w",
+    name: "Worthy Half Sleeve T-Shirt (AVEGL)",
+    countInStock: 10,
   },
   {
-    productName: "Gokulegendary Warrior Dragon Ball Z T Shirt",
+    name: "Gokulegendary Warrior Dragon Ball Z T Shirt",
     image:
       "https://images.bewakoof.com/t540/redwolf-gokulegendary-warrior--dragon-ball-z-official-half-sleeves-t-shirt-391563-1629721983-1.jpg",
     price: "499",
+    countInStock: 10,
   },
 ];
