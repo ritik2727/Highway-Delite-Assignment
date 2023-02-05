e713dff20d56c8c6fe051e354e8b87a71c2f24e8
ritik2727 <ritikjain2727@gmail.com>
2023-02-05 19:18:48 +0530

    deploy to vercel

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ import { notFound, errorHandler } from "./backend/middleware/errorMiddleware.js"
 import morgan from "morgan";
 import ConnectDB from './backend/config/db.js';
 import orderRoutes from "./backend/routes/orderRoutes.js";
+import path from "path";
 
 dotenv.config();
 ConnectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running....");
+// });
 
 
 if (process.env.NODE_ENV === "development") {
@@ -29,7 +30,19 @@ app.use("/api/products", productRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/orders", orderRoutes);
 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} 
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
